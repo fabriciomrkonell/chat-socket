950cfbc2ee45067c9bcf3d5df7aed78a9e5055a3
Lucas Churchill <lucas@agtlucas.com>
2014-08-29 13:37:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var  app = express();
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
